8ddde7a2a1462234907099fc1967887984c4e71d
advatunes <dj-9-1-1@yandex.ru>
2023-06-09 19:30:52 +0300

    update cors

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const helmet = require("helmet");
 const { errors } = require("celebrate");
 const limiter = require("./utils/limiter");
 const routes = require("./routes");
-// const cors = require("cors");
+const cors = require("cors");
 const config = require("./config");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const { STATUS_NOT_FOUND } = require("./utils/errors");
@@ -16,6 +16,12 @@ mongoose.connect(config.mongoUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+app.use(cors({
+  origin: ["https://advatunes.mesto.nomoredomains.monster", "http://localhost:3001"],
+  credentials: true,
+}));
+app.options("*", cors());
 
 app.use(requestLogger);
 app.use(limiter);
